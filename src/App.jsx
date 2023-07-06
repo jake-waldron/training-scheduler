5979ecf0a559d10f2ec1d1ca100349ac3d37cd68
Jake Waldron <jakewaldron@gmail.com>
2023-07-06 11:45:50 -0400

    working example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import getTrainingShifts from './lib/trainings';
 import { easy, hard, medium } from './lib/testSchedules';
-import makePrettyTime from './lib/utils/prettyTime';
 import ShiftButton from './components/ShiftButton';
 import { Fragment } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 const preScheduledShifts = [easy, medium, hard];
 
@@ -16,6 +17,7 @@ function generateEmptyShift() {
 	};
 }
 const shiftStart = [generateEmptyShift(), generateEmptyShift()];
+let originalShifts = [];
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -24,104 +26,140 @@ function App() {
 
 	function handleSubmit(e) {
 		e.preventDefault();
-		console.log('submitted');
-		setTrainingShifts(getTrainingShifts(shifts));
+		const shiftForTrainings = getTrainingShifts(shifts).map((shift) => {
+			return {
+				...shift,
+				color: 'red',
+			};
+		});
+		setTrainingShifts(shiftForTrainings);
+		const updatedShifts = originalShifts.map((shift) => {
+			if (shiftForTrainings.find((trainingShift) => trainingShift.scout === shift.scout)) {
+				return {
+					...shift,
+					color: 'red',
+				};
+			}
+			return shift;
+		});
+		setShifts(updatedShifts);
 	}
 
 	return (
 		<main className='min-h-screen flex flex-col items-center p-20'>
-			<div className='flex gap-10'>
-				{preScheduledShifts.map((shift, idx) => (
-					<ShiftButton
-						key={idx}
-						onClick={() => {
-							setShifts(shift.shifts);
-							setTrainingShifts([]);
-						}}>
-						{shift.name}
-					</ShiftButton>
-				))}
-				<ShiftButton onClick={() => setShifts(shiftStart)}>Custom</ShiftButton>
-			</div>
-
-			{shifts.length > 0 && (
-				// <table className='table-auto border-separate w-1/2'>
-				// 	<thead>
-				// 		<tr>
-				// 			<th>Scout</th>
-				// 			<th>Start</th>
-				// 			<th>End</th>
-				// 		</tr>
-				// 	</thead>
-				// 	<tbody>
-				// 		{shifts.map((shift) => (
-				// 			<tr className='h-4 bg-slate-300 py-4' key={shift.id}>
-				// 				<td>{shift.scout}</td>
-				// 				<td>{makePrettyTime(shift.start)}</td>
-				// 				<td>{makePrettyTime(shift.end)}</td>
-				// 			</tr>
-				// 		))}
-				// 	</tbody>
-				// </table>
-				<div className='grid grid-cols-3 w-1/2 justify-items-center'>
-					<p>Scout</p>
-					<p>Start</p>
-					<p>End</p>
-					{shifts.map((shift) => (
-						<Fragment key={shift.id}>
-							<input
-								className='border border-slate-300 rounded-md p-2 pl-4'
-								type='text'
-								value={shift.scout}
-								onChange={(e) => {
-									const oldShifts = [...shifts];
-									const newShifts = oldShifts.map((oldShift) => {
-										if (oldShift.id === shift.id) {
-											return { ...oldShift, scout: e.target.value };
-										}
-										return oldShift;
-									});
-									setShifts(newShifts);
-								}}
-							/>
-							<input
-								className='border border-slate-300 rounded-md p-1'
-								type='time'
-								value={shift.start}
-								onChange={(e) => {
-									const oldShifts = [...shifts];
-									const newShifts = oldShifts.map((oldShift) => {
-										if (oldShift.id === shift.id) {
-											return { ...oldShift, start: e.target.value };
-										}
-										return oldShift;
-									});
-									setShifts(newShifts);
-								}}
-							/>
-							<input
-								className='border border-slate-300 rounded-md p-1'
-								type='time'
-								value={shift.end}
-								onChange={(e) => {
-									const oldShifts = [...shifts];
-									const newShifts = oldShifts.map((oldShift) => {
-										if (oldShift.id === shift.id) {
-											return { ...oldShift, end: e.target.value };
-										}
-										return oldShift;
-									});
-									setShifts(newShifts);
-								}}
-							/>
-						</Fragment>
-					))}
+			<div className='flex h-auto w-full  justify-center'>
+				<div className='flex flex-col w-full items-center h-full'>
+					<div className='flex gap-10'>
+						{preScheduledShifts.map((shift, idx) => (
+							<ShiftButton
+								key={idx}
+								onClick={() => {
+									originalShifts = [...shift.shifts];
+									setShifts(shift.shifts);
+									setTrainingShifts([]);
+								}}>
+								{shift.name}
+							</ShiftButton>
+						))}
+						<ShiftButton
+							onClick={() => {
+								originalShifts = [...shiftStart];
+								setShifts(shiftStart);
+							}}>
+							Custom
+						</ShiftButton>
+					</div>
+					{shifts.length > 0 && (
+						<div className='grid grid-cols-3 justify-items-center mt-12 gap-y-4'>
+							<p>Scout</p>
+							<p>Start</p>
+							<p>End</p>
+							{shifts.map((shift) => (
+								<Fragment key={shift.id}>
+									<input
+										className='border border-slate-300 rounded-md p-2 pl-4'
+										type='text'
+										value={shift.scout}
+										onChange={(e) => {
+											const oldShifts = [...shifts];
+											const newShifts = oldShifts
+												.map((oldShift) => {
+													if (oldShift.id === shift.id) {
+														return { ...oldShift, scout: e.target.value };
+													}
+													return oldShift;
+												})
+												.map((shift) => {
+													return {
+														...shift,
+														color: 'blue',
+													};
+												});
+											setShifts(newShifts);
+										}}
+									/>
+									<input
+										className='border border-slate-300 rounded-md p-1'
+										type='time'
+										value={shift.start}
+										onChange={(e) => {
+											const oldShifts = [...shifts];
+											const newShifts = oldShifts.map((oldShift) => {
+												if (oldShift.id === shift.id) {
+													return { ...oldShift, start: e.target.value };
+												}
+												return oldShift;
+											});
+											setShifts(newShifts);
+										}}
+									/>
+									<input
+										className='border border-slate-300 rounded-md p-1'
+										type='time'
+										value={shift.end}
+										onChange={(e) => {
+											const oldShifts = [...shifts];
+											const newShifts = oldShifts.map((oldShift) => {
+												if (oldShift.id === shift.id) {
+													return { ...oldShift, end: e.target.value };
+												}
+												return oldShift;
+											});
+											originalShifts = [...newShifts];
+											setShifts(newShifts);
+										}}
+									/>
+								</Fragment>
+							))}
+						</div>
+					)}
+					<div className='flex flex-col items-center'>
+						<ShiftButton onClick={handleSubmit}>Process Schedule</ShiftButton>
+						{trainingShifts.length > 0 && (
+							<p className='text-2xl'>
+								Schedule trainings for : {trainingShifts.map((shift) => shift.scout).join(', ')}
+							</p>
+						)}
+					</div>
 				</div>
-			)}
-			<ShiftButton onClick={handleSubmit}>Process Schedule</ShiftButton>
-			{trainingShifts.length > 0 && (
-				<p>Schedule trainings for : {trainingShifts.map((shift) => shift.scout).join(', ')}</p>
-			)}
+				<div className='w-1/3 mr-40 '>
+					<FullCalendar
+						height={800}
+						plugins={[timeGridPlugin]}
+						initialView='timeGridDay'
+						initialDate={new Date('2023-07-23')}
+						headerToolbar={{ start: '', center: 'title', end: '' }}
+						events={shifts.map((shift) => {
+							return {
+								title: shift.scout,
+								start: new Date(`2023-07-22T${shift.start}`),
+								end: new Date(`2023-07-22T${shift.end}`),
+								color: shift.color,
+							};
+						})}
+					/>
+				</div>
+			</div>
 		</main>
 	);
 }
